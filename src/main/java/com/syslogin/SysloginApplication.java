0e7ceae261180c5636046a44a56d119397c40a3e
MY <my@MYs-MacBook-Air.local>
2022-10-30 14:37:55 +0100

    added pom.xml

---
+++ src/main/java/com/syslogin/SysloginApplication.java
@@ -0,0 +1,13 @@
+package com.syslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syslogin/SysloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syslogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
